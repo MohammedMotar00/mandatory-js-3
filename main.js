c4f6c026fdd24974f8e71653d5bdf15e73216579
Mohammed Motar <mohammed.motar.mm@gmail.com>
2019-12-06 04:36:38 +0100

    Jag skrev om koden på ett renare sätt, kommenterade koden och stylade bättre  --> 100% Done

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
+//  Hämtar Api länk och querySelector min div som kommer vara parent för mina bilder!
 const URL_ALL_BREEDS = 'https://dog.ceo/api/breeds/list/all';
 let divImg = document.querySelector('.images');
 
 
 
-window.onload = function() {
+// när jag laddar om sidan så ska den kunna läsa av window.location.hash och från den rendera bilder
+window.onload = () => {
     if (window.location.hash.includes('-')) {
         let myHash = window.location.hash.substring(1);
 
         let hash0 = myHash.split("-")[0];
         let hash1 = myHash.split("-")[1];
+
+        message(`${hash0} ${hash1}`);
 
         axios
             .get(`https://dog.ceo/api/breed/${hash0}/${hash1}/images/random/3`)
@@ -25,12 +29,14 @@ window.onload = function() {
     } else if (window.location.hash) {
         let hash = window.location.hash.substring(1);
 
+        message(`${hash}`);
+
         axios
             .get(`https://dog.ceo/api/breed/${hash}/images/random/3`)
             .then(res => {
                 res.data.message.forEach(img => {
                     let images = document.createElement('img');
-
+                    console.log(img)
                     images.src = img;
 
                     divImg.appendChild(images);
@@ -38,250 +44,276 @@ window.onload = function() {
             });
 
     } else {
+        message('Random hundar');
+
         axios
             .get(`https://dog.ceo/api/breeds/image/random/3`)
             .then(res => {
                 res.data.message.forEach(img => {
                     let images = document.createElement('img');
 
-                    images.src = img;
-
-                    divImg.appendChild(images);
-                });
-            });
-    };
-};
-
-
-
-function refreshButton() {
-const btn = document.getElementById('random');
-
-btn.addEventListener('click', function(e) {
-    divImg.innerHTML = "";
-    if (window.location.hash.includes('-')) {
-        let myHash = window.location.hash.substring(1);
-
-        let hash0 = myHash.split("-")[0];
-        let hash1 = myHash.split("-")[1];
-
-        axios
-            .get(`https://dog.ceo/api/breed/${hash0}/${hash1}/images/random/3`)
-            .then(res => {
-                //divImg.innerHTML = "";
-                res.data.message.forEach(img => {
-                    let images = document.createElement('img');
+                    //console.log(img)
 
                     images.src = img;
 
                     divImg.appendChild(images);
                 });
             });
-
-    } else if (window.location.hash) {
-        let hash = window.location.hash.substring(1);
-
-        axios
-            .get(`https://dog.ceo/api/breed/${hash}/images/random/3`)
-            .then(res => {
-                //divImg.innerHTML = "";
-                res.data.message.forEach(img => {
-                    let images = document.createElement('img');
-
-                    images.src = img;
-
-                    divImg.appendChild(images);
-                });
-            });
-
-    } else {
-        axios
-            .get(`https://dog.ceo/api/breeds/image/random/3`)
-            .then(res => {
-                //divImg.innerHTML = "";
-                res.data.message.forEach(img => {
-                    let images = document.createElement('img');
-
-                    images.src = img;
-
-                    divImg.appendChild(images);
-                });
-            });
-    };
-});
-};
-
-
-
-
-function createAllBreeds(dogs) {
-let dogDiv = document.createElement('div'),
-    li = document.createElement('li');
-
-dogDiv.classList.add('div-container');
-li.classList.add('first-dog-list');
-li.innerHTML = dogs;
-
-dogDiv.appendChild(li);
-
-renderImg(li);
-
-li.addEventListener('click', function() {
-    let hash = window.location.hash = dogs;
-    getDogId(hash);
-});
-
-return dogDiv;
+    }
 }
 
 
 
-function createAllSubBreeds(dog, div, id) {
-let subDiv = document.createElement('div');
-//console.log(id);
-for (let dogs of dog) {
 
-    let li = document.createElement('li');
+// skapar breeds
+function createAllBreeds(dogs) {
+    let dogDiv = document.createElement('div'),
+        li = document.createElement('li');
 
-    li.classList.add('newLi');
+    dogDiv.classList.add('breed-container');
+    li.classList.add('breed-dogs');
     li.innerHTML = dogs;
 
-    subDiv.appendChild(li);
-    div.appendChild(subDiv);
+    dogDiv.appendChild(li);
 
-    renderSubImg(li);
+    renderImg(li);
 
-    li.addEventListener('click', function() {
+    li.addEventListener('click', () => {
         let hash = window.location.hash = dogs;
-
+        getDogId(hash);
     });
-};
-};
+
+    return dogDiv;
+}
 
 
 
-function renderAllBreeds(dog) {
+
+//skapar sub-Breeeds
+function createAllSubBreeds(dog, div) {
+    let subBreedDiv = document.createElement('div');
+
+    for (let dogs of dog) {
+        let li = document.createElement('li');
+
+        subBreedDiv.classList.add('subBreedDiv')
+        li.classList.add('sub-breed-li');
+        li.innerHTML = dogs;
+
+        subBreedDiv.appendChild(li);
+        div.appendChild(subBreedDiv);
+
+        renderSubBreedImg(li);
+    }
+}
+
+
+
+
+// renderar breeds till DOM
+function render(dog) {
     let ul = document.querySelector('ul');
     ul.innerHTML = "";
 
+
     for (let dogs in dog) {
         let createdDogs = createAllBreeds(dogs);
-        //renderImg(createdDogs);
-
         ul.appendChild(createdDogs);
 
 
-    if (dogs === 'buhund' || dogs === 'bulldog' || dogs === 'bullterrier' || 
-        dogs === 'cattledog' || dogs === 'collie' || dogs === 'corgi' || 
-        dogs === 'dane' || dogs === 'deerhound' || dogs === 'elkhound' ||
-        dogs === 'frise' || dogs === 'greyhound' || dogs === 'hound' || 
-        dogs === 'mastiff' || dogs === 'mountain' || dogs === 'pinscher' ||
-        dogs === 'pointer' || dogs === 'poodle' || dogs === 'retriever' ||
-        dogs === 'ridgeback' || dogs === 'schnauzer' || dogs === 'setter' ||
-        dogs === 'sheepdog' || dogs === 'spaniel' || dogs === 'springer' ||
-        dogs === 'terrier' || dogs === 'waterdog' || dogs === 'wolfhound') {
+        if (dog[dogs].length > 0) {
+            axios
+                .get(`https://dog.ceo/api/breed/${dogs}/list`)
+                .then(res => {
+                    let dog = res.data.message;
 
-        axios
-            .get(`https://dog.ceo/api/breed/${dogs}/list`)
-            .then(res => {
-
-                let dog = res.data.message;
-
-                createAllSubBreeds(dog, createdDogs);
-            });
-        };
-    };
-};
+                    createAllSubBreeds(dog, createdDogs);
+                });
+        }
+    }
+}
 
 
 
+
+// hämtar ut alla hundar
 function getAllDogs() {
     axios
         .get(URL_ALL_BREEDS)
         .then(res => {
-            renderAllBreeds(res.data.message);
+            render(res.data.message);
         });
-};
+}
 
 
 
+
+// denna funktionen kopplas till createAllBreeds() och nyttan är att få window.location.hash
+// denna hash är enbart för breeds
 function getDogId(id) {
     axios
         .get(`https://dog.ceo/api/breed/${id}/list`);
-};
+}
 
 
-function getDogSubId(value1, value2) {
-    //console.log(value1);
-    //console.log(value2);
+
+
+// denna funktionen är samma sak som getDogId()
+//fast här är då hash för sub-breeds och kopplas med  --> renderSubImg()
+function getDogSubBreedId(value1, value2) {
     axios
         .get(`https://dog.ceo/api/breed/${value1}/list`)
         .then(res => {
-            //value2 = res;
             for (let i of res.data.message) {
                 if (i === value2) {
                     value2 = i;
                     window.location.hash = `${value1}-${value2}`;
                 }
             }
-/*
-            value2 = res.data.message;
-            window.location.hash = `${value1}-${value2}`;
-*/
-        })
-};
-
-
-
-function renderImg(dogLi) {
-let li = dogLi;
-
-li.addEventListener('click', function(e) {
-    let value = e.target.textContent;
-
-    axios
-        .get(`https://dog.ceo/api/breed/${value}/images/random/3`)
-        .then(res => {
-            divImg.innerHTML = "";
-
-            res.data.message.forEach(links => {
-                let img = document.createElement('img');
-
-                img.src = links;
-
-                divImg.appendChild(img);
-            });
         });
-});
-};
+}
 
-//document.querySelector('ul').parentElement
 
-function renderSubImg(subLi) {
-let li = subLi;
 
-li.addEventListener('click', function(e) {
-    let value1 = e.target.parentElement.parentElement.firstChild.textContent;
-    let value2 = e.target.textContent;
 
-    getDogSubId(value1, value2);
-    axios
+// renderar bilderna för breeds när jag klickar på en av breeds <li>
+function renderImg(breed) {
+    let li = breed;
+
+    li.addEventListener('click', e => {
+        let value = e.target.textContent;
+
+        message(value);
+
+        axios
+            .get(`https://dog.ceo/api/breed/${value}/images/random/3`)
+            .then(res => {
+                divImg.innerHTML = "";
+
+                res.data.message.forEach(images => {
+                    let img = document.createElement('img');
+
+                    img.src = images;
+
+                    divImg.appendChild(img);
+                })
+            });
+    });
+}
+
+
+
+
+// Renderar subBreed bilderna när man klickar på en av subBreeds <li>
+function renderSubBreedImg(subBreed) {
+    let li = subBreed;
+
+    li.addEventListener('click', e => {
+        let value1 = e.target.parentElement.parentElement.firstChild.textContent,
+            value2 = e.target.textContent;
+
+
+        message(`${value1} ${value2}`);
+
+
+        // jag vill hämta value1 och value2 till min getDogSubBreedId() så jag kan lägga ut hash.
+        getDogSubBreedId(value1, value2);
+
+
+        axios
         .get(`https://dog.ceo/api/breed/${value1}/${value2}/images/random/3`)
         .then(res => {
             divImg.innerHTML = "";
 
-            res.data.message.forEach(links => {
+            res.data.message.forEach(images => {
                 let img = document.createElement('img');
 
-                img.src = links;
+                img.src = images;
 
                 divImg.appendChild(img);
             });
         });
-});
-};
+    })
+}
+
+
+
+
+// funktion refreshButton()  --> när jag klickar på button, så ska bilderna uppdateras
+function refreshButton() {
+    const btn = document.getElementById('random-dog');
+
+    btn.addEventListener('click', () => {
+        divImg.innerHTML = "";
+
+        if (window.location.hash.includes('-')) {
+            let myHash = window.location.hash.substring(1);
+    
+            let hash0 = myHash.split("-")[0];
+            let hash1 = myHash.split("-")[1];
+    
+            message(`${hash0} ${hash1}`);
+    
+            axios
+                .get(`https://dog.ceo/api/breed/${hash0}/${hash1}/images/random/3`)
+                .then(res => {
+                    divImg.innerHTML = "";
+                    res.data.message.forEach(img => {
+                        let images = document.createElement('img');
+    
+                        images.src = img;
+    
+                        divImg.appendChild(images);
+                    });
+                });
+    
+        } else if (window.location.hash) {
+            let hash = window.location.hash.substring(1);
+    
+            message(`${hash}`);
+    
+            axios
+                .get(`https://dog.ceo/api/breed/${hash}/images/random/3`)
+                .then(res => {
+                    divImg.innerHTML = "";
+                    res.data.message.forEach(img => {
+                        let images = document.createElement('img');
+    
+                        images.src = img;
+    
+                        divImg.appendChild(images);
+                    });
+                });
+    
+        } else {
+            message('Random hundar');
+    
+            axios
+                .get(`https://dog.ceo/api/breeds/image/random/3`)
+                .then(res => {
+                    divImg.innerHTML = "";
+                    res.data.message.forEach(img => {
+                        let images = document.createElement('img');
+    
+                        images.src = img;
+    
+                        divImg.appendChild(images);
+                    });
+                });
+        };
+    })
+}
+
+
+
+// function message() kommer visa mig vilken sorts hund det är
+function message(msg) {
+    document.querySelector('.dogType').innerHTML = msg;
+}
+
+
 
 
 refreshButton();
 getAllDogs();
-renderAllBreeds();
+render();
